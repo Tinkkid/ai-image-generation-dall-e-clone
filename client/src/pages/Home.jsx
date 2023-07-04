154f9721a262df4a0e254ca232d919989cf0efdd
Yuliia Kostovynska <y.kostovynska@gmail.com>
2023-07-04 12:04:23 +0300

    Created fetching for render posts

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -17,6 +17,32 @@ const Home = () => {
    const [allPosts, setAllPosts] = useState([]);
    const [searchText, setSearchText] = useState("");
 
+    const fetchPosts = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch('http://localhost:8080/api/v1/post', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (response.ok) {
+        const result = await response.json();
+        setAllPosts(result.data.reverse());
+      }
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
   return (
     <section className='max-w-7xl mx-auto'>
         <div>
@@ -37,7 +63,7 @@ const Home = () => {
                  {searchText && <h2 className='font-medium text-[#666e75] text-xl mb-3'>
                     Showing results for <span className='text-[#222328]'>{searchText}</span></h2>}
                  <div className='grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3 '>
-                    {searchText? (<RenderCards data={[]} title="No search results found"/>) :(<RenderCards data={[]} title="No posts found"/>)
+                    {searchText? (<RenderCards data={[]} title="No search results found"/>) :(<RenderCards data={allPosts} title="No posts found"/>)
                   }
                  </div>
            </>)}
