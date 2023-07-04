c6e3ce479eb99609666573d6e01b188718bbc09f
Yuliia Kostovynska <y.kostovynska@gmail.com>
2023-07-04 12:23:27 +0300

    Added props

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -95,7 +95,7 @@ const Home = () => {
             )}
             <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3 ">
               {searchText ? (
-                <RenderCards data={[]} title="No search results found" />
+                <RenderCards data={searchedResults} title="No search results found" />
               ) : (
                 <RenderCards data={allPosts} title="No posts found" />
               )}
