0f4d32beb3ac37493fd923bfab1da25ecb7cdd97
Yuliia Kostovynska <y.kostovynska@gmail.com>
2023-07-05 12:49:30 +0300

    Changed localhost adress for test

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,8 @@ import React, { useEffect, useState } from "react";
 import { Card, FormField, Loader } from "../components";
 
 const RenderCards = ({ data, title }) => {
-  if (data?.length > 0) {
+   if (data?.length > 0) {
+     console.log(data)
     return data.map((post) => <Card key={post._id} {...post} />);
   }
 
@@ -20,32 +21,32 @@ const Home = () => {
   const [searchTimeout, setSearchTimeout] = useState(null);
   const [searchedResults, setSearchedResults] = useState(null);
 
-  const fetchPosts = async () => {
-    setLoading(true);
+   useEffect(() => {
+    const fetchPosts = async () =>{
+      setLoading(true);
 
-    try {
-      const response = await fetch(
-        "https://dalle-cc2a.onrender.com/api/v1/post",
-        {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
+      try {
+        const response = await fetch(
+          "http://localhost:8080/api/v1/post",
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        console.log(response);
+        if (response.ok) {
+          
+          const result = await response.json();
+          setAllPosts(result.data.reverse());
         }
-      );
-
-      if (response.ok) {
-        const result = await response.json();
-        setAllPosts(result.data.reverse());
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
       }
-    } catch (err) {
-      alert(err);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  useEffect(() => {
+    };
     fetchPosts();
   }, []);
 
