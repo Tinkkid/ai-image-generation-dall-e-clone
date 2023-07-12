12a4485f66067cc46b55edc0d6fb821dcf9df086
Yuliia Kostovynska <y.kostovynska@gmail.com>
2023-07-12 14:23:51 +0300

    Tested cors

--- server/index.js
+++ server/index.js
@@ -10,12 +10,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({
-    origin: 'http://localhost:5173/', // use your actual domain name (or localhost), using * is not recommended
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
-    credentials: true
-}))
+// app.use(cors())
 app.use(express.json({ limit: '50mb' }));
 
 app.use('/api/v1/post', postRoutes);
